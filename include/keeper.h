eeec2af9f3789abb978e7f564d7056873e65b4df
schrodingerzhu <i@zhuyi.fan>
2020-02-06 00:19:48 +0800

    update headerfile

--- include/keeper.h
+++ include/keeper.h
@@ -6,15 +6,17 @@
 #define KEEPERCORE_INCLUDE_H
 #include <cstddef>
 typedef struct {
-    size_t length;
-    size_t capacity;
-    const char** vector;
+  size_t length;
+  size_t capacity;
+  const char** vector;
 } result_t;
 
 extern "C" result_t fetch_list(const char* password);
 extern "C" result_t fetch_password(const char* name, const char* password);
 extern "C" result_t add_password(const char* name, const char* content, const char* password);
 extern "C" result_t delete_password(const char* name, const char* password);
-extern "C" result_t clean_result(result_t res);
-extern "C" size_t botan_version();
+extern "C" void clean_result(result_t res);
+extern "C" result_t botan_version();
+extern "C" result_t get_server();
+extern "C" result_t generate_password(const char* name, const char* password);
 #endif // KEEPERCORE_INCLUDE_INCLUDE_H
